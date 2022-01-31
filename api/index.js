c2595221eb0fa1e4c0448c45e05733d1025fdb6c
Laurent Brand <laurentbrand19@gmail.com>
2022-01-31 13:18:01 +0100

    added stickers with api functions to receive stickers within color range

--- api/index.js
+++ api/index.js
@@ -42,6 +42,58 @@ app.get('/api/skin/:icon_url/getColors', async (req, res) => {
     
 });
 
+app.get("/api/stickers", (req, res) => {
+    try {
+        var rawData = fs.readFileSync('./stickers.json')
+        var data = JSON.parse(rawData)
+        res.send(data)
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+app.get("/api/stickers/:color_range", (req, res) => { // return stickers within the color range
+    var color_range = req.params.color_range
+    var selectedStickers = []
+    try {
+        var rawData = fs.readFileSync('./stickers.json')
+        var stickers = JSON.parse(rawData)
+        for(var i = 0; i < stickers.length; i++) {
+            if(stickers[i]['colors'] WITHIN color_range){
+                selectedStickers.push(stickers[i])
+            }
+        }
+        res.send(selectedStickers)
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+// needs to be tested ---
+app.get('/api/stickers/setColors', async (req, res) => { 
+    var rawData = fs.readFileSync('./stickers.json')
+    var stickers = JSON.parse(rawData)
+    for(var i = 0; i < stickers.length; i++) {
+        let icon_url = stickers[i].icon_url;
+        let url = `https://steamcommunity-a.akamaihd.net/economy/image/${icon_url}`
+        const request = await axios.get(url, {
+            responseType: "arraybuffer",
+        });
+        var buffer = Buffer.from(request.data);
+        getColors(buffer, 'image/png').then(colors => {
+            for (let i = 0; i < colors.length; i++) {
+                const [r,g,b,a] = colors[i]['_rgb'];
+                colors[i]['rgba'] = `rgba(${r},${g},${b},${a})`
+                delete colors[i]['_rgb']
+            }
+            stickers[i]['colors'] = colors;
+            fs.writeFileSync('./stickers.json', stickers)
+        })
+    }  
+    
+});
+// ---
+
 app.listen(4000, () => {
     console.log('Server running on port 4000');
 });
--- frontend/src/SkinService.js
+++ frontend/src/SkinService.js
@@ -3,14 +3,16 @@ require('dotenv').config();
 
 const axios = require('axios');
 
-const skins_url = 'http://192.168.178.66:4000/api/skins'
-const skin_base_url = 'http://192.168.178.66:4000/api/skin/'
+const SKINS_URL = 'http://localhost:4000/api/skins'
+const SKIN_BASE_URL = 'http://localhost:4000/api/skin/'
+const STICKER_URL = 'http://localhost:4000/api/stickers'
+const STICKER_BASE_URL = 'http://localhost:4000/api/stickers/'
 
 export default class SkinService {
 
     static async getSkins() {
         try {
-            const response = await axios.get(skins_url);
+            const response = await axios.get(SKINS_URL);
             return response.data.items_list;
         } catch (error) {
             console.log(error);
@@ -19,7 +21,27 @@ export default class SkinService {
 
     static async getSkinColors(icon_url) {
         try {
-            const response = await axios.get(skin_base_url + icon_url + '/getColors');
+            const response = await axios.get(SKIN_BASE_URL + icon_url + '/getColors');
+            return response.data;
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    static async getStickers() {
+        try {
+            const response = await axios.get(STICKER_URL);
+            return response.data;
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    static async getStickersWithinColorRange(color_range) {
+        try {
+            const response = await axios.get(STICKER_BASE_URL+color_range);
             return response.data;
 
         } catch (error) {
